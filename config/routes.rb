a280223a82fd1dcffce9e1813bff889a8a53b0c0
tmorg97 <tmorg@umich.edu>
2025-07-26 14:52:33 -0400

    fixed lint issues

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-   root "pages#home"
+  root "pages#home"
 end
